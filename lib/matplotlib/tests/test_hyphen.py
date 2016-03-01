3a6b89ae5dcca0b5baefef6d17cd6b5a9599bebc
wangx173 <wangx173@wifihost-96-235.wireless.utsc.utoronto.ca>
2016-03-01 18:33:10 -0500

    added more test cases for hyphen

--- lib/matplotlib/tests/test_hyphen.py
+++ lib/matplotlib/tests/test_hyphen.py
@@ -1,10 +1,27 @@
 import matplotlib.pyplot as plt
 from matplotlib.testing.decorators import image_comparison
 
-@image_comparison(baseline_images=['hyphen_type'],
+@image_comparison(baseline_images=['hyphen_math'],
                   extensions=['png'])
-def test_hyphen():
-	math = u'math_part: ${-}$'
-	non_math = u'non_math_part: -'
-	plt.text(0.5, 0.5, non_math)
-	plt.text(0.5, 0.3, non_math + '  ' + math)
+def test_hyphen_in_math():
+	text = u'${-}$'
+	plt.text(0.5, 0.5, text)
+
+
+@image_comparison(baseline_images=['hyphen_non_math'],
+                  extensions=['png'])
+def test_hyphen_in_nonmath():
+	text = u'-'
+	plt.text(0.5, 0.5, text)
+
+
+
+@image_comparison(baseline_images=['hyphen_mixed'],
+                  extensions=['png'])
+def test_hyphen_in_mixed():
+	text = u'math part: ${-}$; non-math part: -'
+	plt.text(0.5, 0.5, text)
+
+text = u'${-}$'
+plt.text(0.5, 0.5, text)
+plt.show()
